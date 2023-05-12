0bc4ba4c5d32a4e468e63858cf0e2ea322a74d2c
Roberto Umbelino <beto.umbelino@gmail.com>
2023-05-11 22:00:06 -0300

    feat: added script to generate new version

--- src/generate-version.ts
+++ src/generate-version.ts
@@ -1,7 +1,68 @@
+import packageJson from '../package.json'
 import { execSync } from 'child_process'
 
+/**
+ * Get not pushed commits.
+ */
 const response = execSync(`git cherry -v origin/master master`)
 
-console.log(response.toString())
+/**
+ * Valid terms.
+ */
+const TERMS = ['feat', 'fix', 'chore']
 
-console.log('hi')
+/**
+ * Get current version.
+ */
+const [major, minor, patch] = packageJson.version
+  .split('.')
+  .map(value => +value)
+
+/**
+ * Get new version.
+ */
+const newVersion = response
+  .toString()
+  .split('\n')
+  .reduce(
+    (version, message) => {
+      const [firstPart] = message.split(':')
+      const [, , term] = firstPart.split(' ')
+
+      /**
+       * Term not found,
+       */
+      if (!TERMS.includes(term)) {
+        return version
+      }
+
+      /**
+       * Is chore.
+       */
+      if (term === 'chore') return { ...version, patch: version.patch + 1 }
+
+      /**
+       * Is fix.
+       */
+      if (term === 'fix') return { ...version, patch: version.patch + 1 }
+
+      /**
+       * Is feat.
+       */
+      if (term === 'feat')
+        return { ...version, minor: version.minor + 1, patch: 0 }
+
+      return version
+    },
+    { major, minor, patch }
+  )
+
+/**
+ * Normalize version.
+ */
+const normalizedVersion = `${newVersion.major}.${newVersion.minor}.${newVersion.patch}`
+
+/**
+ * Generate new version.
+ */
+execSync(`git version --new-version ${normalizedVersion}`)
